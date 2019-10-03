ef9380a89a93a01727dba24b1eb0876a9c09ecf5
Abdulrahman Ali Hussain <AbdulrahmanAliHussainM@gmail.com>
2019-10-03 14:39:04 +0200

    Added Show-Primes solution

---
+++ Show Primes/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+showPrimes(10);  // 2, 3, 5, 7
+
+function showPrimes(limit) {
+    for (let i = 2; i <= limit; i++)
+        if (isPrime(i))
+            console.log(i);
+}
+
+function isPrime(number) {
+    for (let j = 2; j < number; j++)
+        if (number % j == 0)
+            return false;
+
+    return true;
+}
