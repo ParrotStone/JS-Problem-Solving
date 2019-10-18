7208fbd3bd205da0f06dbad955bfeb2b9cfed4fc
Abdulrahmn Ali Hussain <abdulrahmanalihussainM@gmail.com>
2019-10-19 00:47:46 +0200

    Added Jewels_Stones problem solution

---
+++ Jewels_Stones/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Problem Description at -> https://leetcode.com/problems/jewels-and-stones/
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    // First approach
+    let count = 0;
+    for (let i = 0; i < J.length; i++) {
+        count += [...S].reduce((a, c) => {
+            return (J[i] == c) ? a + 1 : a;
+
+        }, 0);
+    }
+    
+    return count;
+};
+
+// var numJewelsInStones = function(J, S) {
+//     // Second approach
+//     let count = 0;
+//     for (let i = 0; i < J.length; i++) {
+//         for (let j = 0; j < S.length; j++) {
+//             if (J[i] == S[j]) {
+//                 count++;
+//             }
+//         }
+//     }
+    
+//     return count;
+// };
+
+
+console.log(numJewelsInStones('aA', 'aAAbbbb'));  // 3
+console.log(numJewelsInStones('ebd', 'bbb'));  // 3
