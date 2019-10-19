10c8f3efa5588d5966d32ae7d73b6ac6e46c8e2a
Abdulrahman Ali Hussain <AbdulrahmanAliHussainM@gmail.com>
2019-10-19 21:18:12 +0200

    Added power-of-two problem solution

---
+++ power-of-two/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    if (n <= 0) return false;
+
+    let powerNum = 0;
+    while (2 ** powerNum <= n) {
+        if (2 ** powerNum == n)
+            return true;
+
+        powerNum++;
+    }
+
+    return false;
+};
