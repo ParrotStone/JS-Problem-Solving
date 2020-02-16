f739b3964f729606ed7882a0caf8084ee23017f1
Abdulrahmn Ali Hussain <ParrotStone@gmail.com>
2020-02-16 21:18:45 +0200

    Added divisible-sum pairs problem solution

---
+++ divisible-sum-pairs/main.js
@@ -0,0 +1,18 @@
+// Problem Description at ->> https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+// Brute force solution, preferred over recursion!
+
+console.log(divisibleSumPairs(6, 5, [1, 2, 3, 4, 5, 6]));  // 3
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));  // 5
+
+function divisibleSumPairs(n, k, ar) {
+    let arrOfPairs = [];
+    for (let m = 0; m < n; m++) {
+        for (let o = m + 1; o < n; o++) {
+            if ((ar[m] + ar[o]) % k === 0) arrOfPairs.push({ i: m, j: m + 1 });
+        }
+    }
+
+    return arrOfPairs.length;
+}
+
