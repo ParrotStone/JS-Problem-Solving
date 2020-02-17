eb219069d46c6ad38b11112a8a7e893498aaa553
Abdulrahmn Ali Hussain <ParrotStone@gmail.com>
2020-02-17 13:28:48 +0200

    Added birthday-chocolate problem solution

---
+++ birthday-chocolate/main.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// Problem Description at ->> https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+// Test cases
+console.log(birthday([2, 2, 1, 3, 2], 4, 2)); // 2
+// console.log(birthday([1, 2, 1, 3, 2], 3, 2)); // 2
+
+function birthday(s, d, m) {
+  if (s.length === m && s[0] === d) return 1;
+
+  let numOfWays = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let monthLength = 1;
+    let sum = s[i];
+    for (let j = i + 1; j < s.length; j++) {
+      monthLength++;
+      sum += s[j];
+      if (sum === d && monthLength === m) {
+        numOfWays++;
+      }
+    }
+  }
+
+  return numOfWays;
+}
