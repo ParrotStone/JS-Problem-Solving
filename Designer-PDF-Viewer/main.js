ea61293c0afae94956dcdd14d6c0eed70f58e341
Abdulrahmn Ali Hussain <ParrotStone@gmail.com>
2020-02-26 01:02:17 +0200

    Added Designer-PDF-Viewer problem solution

---
+++ Designer-PDF-Viewer/main.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+// Problem Description ->> https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+console.log(
+  designerPdfViewer(
+    [
+      1,
+      3,
+      1,
+      3,
+      1,
+      4,
+      1,
+      3,
+      2,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ],
+    "abc"
+  )
+); // 9
+
+console.log(
+  designerPdfViewer(
+    [
+      1,
+      3,
+      1,
+      3,
+      1,
+      4,
+      1,
+      3,
+      2,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      7
+    ],
+    "zaba"
+  )
+); // 28
+
+function designerPdfViewer(h, word) {
+  const chars = [];
+  for (let i = 0; i < word.length; i++) {
+    const charPosition = word.charCodeAt(i) - 97;
+    chars.push(h[charPosition]);
+  }
+
+  return Math.max(...chars) * chars.length;
+}
