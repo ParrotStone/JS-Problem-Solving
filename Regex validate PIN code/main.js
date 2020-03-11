cbd7b7a160227ee60cb8d72a0ca3940aa5dcf262
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-11 17:03:21 +0200

    Added Some Code-Wars problem solution

---
+++ Regex validate PIN code/main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// Problem Description at -> https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
+function validatePIN (pin) {
+  return RegExp(/^\d{4}$|^\d{6}$/).test(pin);
+}
+
+
+console.log(validatePIN("1234")); // true
+console.log(validatePIN("12345")); // false
+console.log(validatePIN("a234")); // false
