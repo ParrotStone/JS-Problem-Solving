d248c9cb347dd467956edc29f57d201024aadeff
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-11 17:54:35 +0200

    Update main.js

--- Regex validate PIN code/main.js
+++ Regex validate PIN code/main.js
@@ -3,6 +3,8 @@
 // Problem Description at -> https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
 function validatePIN (pin) {
   return RegExp(/^\d{4}$|^\d{6}$/).test(pin);
+  // OR
+  // return /^(\d{4}|\d{6})$/gmi.test(pin);
 }
 
 
