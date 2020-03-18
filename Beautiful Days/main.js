532f57bdacddc0183dd50e9f607fef29cbf9ce30
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-18 17:29:50 +0200

    Added Beautiful-Days problem solution

---
+++ Beautiful Days/main.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// Problem Description at -> https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+
+
+function beautifulDays(i, j, k) {
+  let numOfBeautifulDays = 0;
+  for (let start = i; start <= j; start++) {
+    const reverse = Number(String(start).split('').reverse().join(''));
+    if ((start - reverse) % k == 0) numOfBeautifulDays++;
+  }
+
+  return numOfBeautifulDays;
+}
+
+console.log(beautifulDays(20, 23, 6)); // 2
