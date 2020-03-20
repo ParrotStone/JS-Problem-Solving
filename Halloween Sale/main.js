2f808b56b9d657e073b0bf307901e09bc3e18676
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-20 06:23:39 +0200

    Added Halloween-Sale problem solution

---
+++ Halloween Sale/main.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// Problem Description at -> https://www.hackerrank.com/challenges/halloween-sale/problem
+
+function howManyGames(p, d, m, s) {
+  let numOfGames = 0;
+  while (s >= m && (s - p) >= 0) {
+    s -= p;
+    p = ((p - d) <= m) ? m : (p - d);
+    numOfGames++;
+  }
+
+  return numOfGames;
+}
+
+// test cases
+// console.log(howManyGames(20, 3, 6, 80)); // 6
+// console.log(howManyGames(20, 3, 6, 85)); // 7
+// console.log(howManyGames(30, 4, 5, 120)); // 6
+// console.log(howManyGames(30, 5, 3, 120)); // 11
+console.log(howManyGames(50, 5, 3, 120)); // 2
