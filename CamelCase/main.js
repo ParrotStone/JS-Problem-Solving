f847968e92e84d6bafe34674f181bf0775185c64
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-21 00:02:52 +0200

    Update main.js

--- CamelCase/main.js
+++ CamelCase/main.js
@@ -3,9 +3,6 @@
 // Problem Description at -> https://www.hackerrank.com/challenges/camelcase/problem
 
 function camelcase(s) {
-   // Check if the string is empty
-   if (!s.length) return 0;
-
    let numOfWords = 1;
    for (let i = 0; i < s.length; i++)
      if (s[i].charCodeAt(0) >= 65 && s[i].charCodeAt(0) <= 90) numOfWords++;
@@ -15,6 +12,10 @@ function camelcase(s) {
   // // One linear solution - credit goes to (https://www.hackerrank.com/sblashkov)
   // // Count the number of captial letters + 1 => number of words
   // return s.length - s.replace(/[A-Z]/g, '').length + 1;
+
+  // // Another solution - credit goes to (https://www.hackerrank.com/AllisonP)
+  // const words = s.split(/[A-Z]/);
+  // return words.length;
 }
 
 console.log(camelcase('oneTwoThree')); // 3
