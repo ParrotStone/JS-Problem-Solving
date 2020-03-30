da5c9528a964ddde4c5367befe359a7d3c48fc9e
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2020-03-30 10:35:47 +0200

    Update main.js

--- Caesar Cipher/main.js
+++ Caesar Cipher/main.js
@@ -34,7 +34,7 @@ function caesarCipher(s, k) {
 // console.log(caesarCipher('This-is-a-string', 0)); // This-is-a-string
 console.log(
 caesarCipher(
-'DNFjxo?b5h*5<LWbgs6?V5{3M].1hG)pv1VWq4(!][DZ3G)riSJ.CmUj9]7Gzl?VyeJ2dIPEW4GYW*scT8(vhu9wCr]q!7eyaoy.', 100));
+'DNFjxo?b5h*5<LWbgs6?V5{3M].1hG)pv1VWq4(!][DZ3G)riSJ.CmUj9]7Gzl?VyeJ2dIPEW4GYW*scT8(vhu9wCr]q!7eyaoy.', 45));
 // WGYcqh?u5a*5<EPuzl6?O5{3F].1aZ)io1OPj4(!][WS3Z)kbLC.VfNc9]7Zse?OrxC2wBIXP4ZRP*lvM8(oan9pVk]j!7xrthr.
 // console.log(caesarCipher('w', 100)); // v
 // console.log(caesarCipher('x', 55)); // a
